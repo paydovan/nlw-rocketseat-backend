4968b1f67b38b0f9e830e4034fd7d9ddb6c46c23
João Pedro Marques padovan <jpmpadovan@gmail.com>
2024-07-11 14:06:02 -0300

    Aula 2 nlw

---
+++ src/server.ts
@@ -0,0 +1,32 @@
+import fastify from "fastify";
+import cors from '@fastify/cors'
+import { createTrip } from "./routes/create-trip";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { confirmTrip } from "./routes/confirm-trip.ts";
+import { confirmParticipants } from "./routes/confirm-participant";
+import { createActivity } from "./routes/create-activity";
+import { getActivities } from "./routes/get-activities";
+import { createLink } from "./routes/create-link";
+import { getLinks } from "./routes/get-links";
+
+const app = fastify()
+
+app.register(cors, {
+   origin: '*',
+   
+})
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createTrip)
+app.register(confirmTrip)
+app.register(confirmParticipants)
+app.register(createActivity)
+app.register(getActivities)
+app.register(createLink)
+app.register(getLinks)
+
+app.listen({ port: 3333 }).then(() => {
+    console.log('Server running!');
+})
